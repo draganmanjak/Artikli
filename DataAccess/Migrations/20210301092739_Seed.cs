757d52bc78feef005e8947f8c2a25f4e390086a3
Dragan Manjak <dragan.manjak@etf.ues.rs.ba>
2021-03-01 10:27:50 +0100

    seed

---
+++ DataAccess/Migrations/20210301092739_Seed.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class Seed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "JediniceMjere",
+                columns: new[] { "PkJedinicaMjereId", "Naziv" },
+                values: new object[,]
+                {
+                    { 1L, "kg" },
+                    { 2L, "l" },
+                    { 3L, "m" },
+                    { 4L, "cm" },
+                    { 5L, "dm" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "JediniceMjere",
+                keyColumn: "PkJedinicaMjereId",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "JediniceMjere",
+                keyColumn: "PkJedinicaMjereId",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "JediniceMjere",
+                keyColumn: "PkJedinicaMjereId",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "JediniceMjere",
+                keyColumn: "PkJedinicaMjereId",
+                keyValue: 4L);
+
+            migrationBuilder.DeleteData(
+                table: "JediniceMjere",
+                keyColumn: "PkJedinicaMjereId",
+                keyValue: 5L);
+        }
+    }
+}
